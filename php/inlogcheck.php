ec79202c6f0716f16dd5dd6b5b33e9f6754547d4
bjoreno <bjodegens@hotmail.com>
2022-05-11 09:57:15 +0200

    php bijgewerkt

--- php/inlogcheck.php
+++ php/inlogcheck.php
@@ -1,21 +1,21 @@
 <?php
     session_start();
-    if isset($_POST['login']){
-    require_once ('connect.php');
-    $message="";
+    if (isset($_POST['login'])){
     if(count($_POST)>0) {
-        $con = mysqli_connect('localhost','root','','miniCrud') or die('Unable To connect');
-        $result = mysqli_query($con,"SELECT * FROM login_user WHERE user_name='" . $_POST["user_name"] . "' and password = '". $_POST["password"]."'");
+        $con = mysqli_connect('localhost','root','','crud') or die('Unable To connect');
+        $result = mysqli_query($con,"SELECT * FROM inlogpage WHERE inlogname ='" . $_POST["name"] . "' and password = '". $_POST["password"]."'");
         $row  = mysqli_fetch_array($result);
         if(is_array($row)) {
-        $_SESSION["id"] = $row['id'];
+        $_SESSION["ID"] = $row['ID'];
         $_SESSION["name"] = $row['name'];
         } else {
          $message = "Invalid Username or Password!";
         }
     }
-    if(isset($_SESSION["id"])) {
+    if(isset($_SESSION["ID"])) {
     header("Location:adminpage.php");
-    }else header("Location:inlog.php")
-}else header("Location:inlog.php")
+    }
+}   else{
+    header("Location:inlog.php");
+    exit();}
 ?>
