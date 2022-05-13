67677417c3110b94a88fd95f559ee9293627cc24
bjoreno <bjodegens@hotmail.com>
2022-05-13 12:22:45 +0200

    inlogcheck.php aangemaakt

--- php/inlog.php
+++ php/inlog.php
@@ -1,19 +1,22 @@
-<?php $message="";?>
+<?php 
+$message="";
+
+?>
 <html>
-<head>
-<title>User Login</title>
-</head>
-<body>
-<form name="frmUser" method="post" action="inlogcheck.php" align="center">
-<div class="message"><?php if($message!="") { echo $message; } ?></div>
-<h3 align="center">Enter Login Details</h3>
- Username:<br>
- <input type="text" name="name">
- <br>
- Password:<br>
-<input type="password" name="password">
-<br><br>
-<input type="submit" name="login" value="Submit">
-</form>
-</body>
+    <head>
+        <title>User Login</title>
+    </head>
+    <body>
+        <form name="frmUser" method="post" action="inlogcheck.php" align="center">
+    <div class="message"><?php if($message!="") { echo $message; } ?></div>
+    <h3 align="center">Enter Login Details</h3>
+    Username:<br>
+    <input type="text" name="name">
+    <br>
+    Password:<br>
+    <input type="password" name="password">
+    <br><br>
+    <input type="submit" name="login" value="Submit">
+    </form>
+    </body>
 </html>
