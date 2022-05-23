fd8e86d37e523e9e0ace19d4c1c6ae2dd48a45a4
Suhaayb <suhaaybahmed@hotmail.com>
2022-05-23 14:27:42 +0200

    SuhaaybV2

--- php/adminpage.php
+++ php/adminpage.php
@@ -25,29 +25,113 @@
         #bar1{
             background-color:darkred;
             height:175px;
+            display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    align-content: flex-end;
+           
+        }
+        #button1{
+            border: 2px solid #04AA6D;
+    padding: 0px 0px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 12px;
+    margin-left: 0px;
+    width: 186px;
+    margin-bottom: 4px;
+
         }
         #bar2{
             background-color:darkblue;
             height:175px;
+            display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    align-content: flex-end;
+        }
+        #button2{
+            border: 2px solid #04AA6D;
+    padding: 0px 0px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 12px;
+    margin-left: 0px;
+    width: 186px;
+    margin-bottom: 4px;
+
         }
         #bar3{
             background-color:darkorange;
             height:175px;
+            display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    align-content: flex-end;
+        }
+        #button3{
+            border: 2px solid #04AA6D;
+            padding: 0px 0px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 12px;
+            margin-left: 0px;
+            width: 186px;
+            margin-bottom: 4px;
+
         }
         #bar4{
-            background-color:darkyellow;
+            background-color:wheat;
             height:175px;
+            display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    align-content: flex-end;
         }
+        #button4{
+            border: 2px solid #04AA6D;
+    padding: 0px 0px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 12px;
+    margin-left: 0px;
+    width: 186px;
+    margin-bottom: 4px;
 
+        }
     </style>
 </head>
 <body style="display: flex;align-content: center;justify-content: center;">
     <div class="mid">
         <div class="midbar">
-            <div id="bar1"></div>
-            <div id="bar2"></div>
-            <div id="bar3"></div>
-            <div id="bar4"></div>
+            <div id="bar1">
+            <a href="adminpage.php" id="button1">
+                <h1>Profile</h1>
+            </a>
+            </div>
+            <div id="bar2">
+            <a href="dashboard.php" id="button2">
+                <h1>Dashboard</h1>
+    </a>
+            </div>
+            <div id="bar3">
+            <a href="reserveren.php" id="button3">
+                <h1>reserveren</h1>
+    </a>
+            </div>
+            <div id="bar4">
+            <a href="logout.php" id="button4">
+                <h1>Logout</h1>
+    </a>
+            </div>
         </div>
         <div class="centermid"></div>
     </div>
