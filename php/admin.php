e5406946111214382cc12f6d591bfbf50b674f44
bjoreno <bjodegens@hotmail.com>
2022-05-30 13:37:46 +0200

    merge conflict fix

--- php/admin.php
+++ php/admin.php
@@ -1,21 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/backend.css">
-    <title>Document</title>
-</head>
-<body>
-    <main>
-        <div class="navbar">
-            <div class="barone"></div>
-            <div class="bartwo"></div>
-            <div class="barthree"></div>
-            <div class="barfour"></div>
-        </div>
-        <div class="infobox"></div>
-    </main>
-</body>
-</html>
+<?php
+session_start();
+    if($_SESSION["admin"] == 1) {
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/backend.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <main>
+            <div class="navbar">
+                <div class="barone"></div>
+                <div class="bartwo"></div>
+                <div class="barthree"></div>
+                <div class="barfour">
+                    <a href="userpage.php">Ga naar userpage</a>
+                </div>
+            </div>
+            <div class="infobox"></div>
+        </main>
+    </body>
+    </html>
+<?php }else header("Location:../inlog.php") ?>
