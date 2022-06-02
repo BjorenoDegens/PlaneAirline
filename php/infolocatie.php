c391ce226dd5cb92ae5bdf226d27d9d25438335b
Suhaayb <suhaaybahmed@hotmail.com>
2022-06-02 10:32:27 +0200

    userpage

---
+++ php/infolocatie.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Josefin+Sans:wght@300&display=swap" rel="stylesheet">
+    <link href="contact-form.css" rel="stylesheet">
+  </head>
+</head>
+<body>
+<header>
+      <div class="box-left">
+        <img
+          class="logo"
+          src="Afbeeldingen/Officieel_logo_CRUD_-removebg-preview.png"
+          alt=""
+        />
+      </div>
+      <div class="box-main">
+        <div class="box">
+        <div class="text">
+          <div class="form-btn">
+            <a class="submit-btn1" href="overons.php" target="_parent" rel="nofollow">	Over ons </a>
+          </div>
+          <div class="form-btn">
+            <a class="submit-btn1" href="contact.php" target="_parent" rel="nofollow"> Informatie over locaties  </a>
+          </div>
+          <div class="form-btn">
+          <a class="submit-btn1" href="contact.php" target="_parent" rel="nofollow">Contact</a>
+        </div>
+        </div>
+        </div>
+      </div>
+      <div class="box-right">
+        <div class="box">
+          <div class="search">
+            <div class="icon"></div>
+            <div class="input">
+              <input type="text" placeholder="Zoeken" id="mysearch" />
+            </div>
+            <span
+              class="clear"
+              onclick="document.getElementById
+            ('mysearch').value = ''"
+            ></span>
+          </div>
+        </div>
+        <div class="box">
+          <div class="box-logo-login">
+            <a href="inlog.php"
+              ><img
+                src="Afbeeldingen/user-icon-png-transparent-17.jpg"
+                target="_parent"
+                alt=""
+            /></a>
+          </div>
+        </div>
+      </div>
+    </header>
+</body>
+</html>
